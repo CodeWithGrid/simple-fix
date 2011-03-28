ca8a09a7385f0ed2ea986c5c754f1fadc44cdded
nateluzod <nate.luzod@mac.com>
2011-03-28 14:48:50 -0400

    cleaning up js

--- jquery.simplefix.js
+++ jquery.simplefix.js
@@ -1,30 +1,30 @@
 (function($){
 	$.fn.simpleFix = function(){
 	
-		// Get element's original attributes (to restore)
-		var $elementToFix						=	$(this); 
-		var $elementInitialOffset		=	Math.floor($elementToFix.offset().top);			
-		var $elementInitialPos  		= $($elementToFix).css("position");
+	// Get element's original attributes (to restore)
+	var $elementToFix						=	$(this); 
+	var $elementInitialOffset		=	Math.floor($elementToFix.offset().top);			
+	var $elementInitialPos			=	$($elementToFix).css("position");
 	
-		$(window).scroll(function(){
+	$(window).scroll(function(){
+	
+		// Figure out when it hits the top
+		var $wrapperScrollTop				=	$(document).scrollTop();
 		
-			// Figure out when it hits the top
-			var $wrapperScrollTop 	= $(document).scrollTop();
+		// Fix it in place
+		if($wrapperScrollTop > $elementInitialOffset){
+			$($($elementToFix)).css({
+				"position"	:	"fixed",
+				"top"				:	0
+			});
 			
-			// Fix it in place
-			if($wrapperScrollTop > $elementInitialOffset){
-				$($($elementToFix)).css({
-					"position"	: "fixed",
-					"top"				:	0
-				});
+		// Put it back when you're done	
 			} else {
-			
-				// Put it back when you're done
 				$($($elementToFix)).css({
-					"position"	: $elementInitialPos,
+					"position"	:	$elementInitialPos,
 					"top"				:	$elementInitialOffset
 				});				
-			};				
+			};
 		});	
 	};
 })(jQuery);
